c2a8058f44cbf6fe5142ecc6d08901915e8a0dc4
DoinaToderici <doina.toderici@gmail.com>
2019-10-02 11:14:08 +0200

    Add CSS

--- js/index.js
+++ js/index.js
@@ -13,7 +13,12 @@ async function getUsers() {
 function displayList(response) {
   let list = "";
   response.forEach(function(user) {
-    list = list + `<li>${user.name} - ${user.email} </li>`;
+    list =
+      list +
+      `<li><p>${user.name}</p><div class="contact"> ${user.email}<br> ${user.phone}<br> ${user.website}<div> </li>`;
   });
   usersList.innerHTML = list;
 }
+
+//1. scrii toate functiile din parametri in es6
+//2. punel pe git fara comentarii
